21248ccba555c792e1c0e0595946b3f1e9571db8
Javier Garay <javier.garay@2brains.cl>
2020-09-08 19:10:56 -0300

    37. Básicos de JSX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function Header() {
+    const edad = 30;
     return(
-        <h1>Desde el header</h1>
+        <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
     )
 }
 
